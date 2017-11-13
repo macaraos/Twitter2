aceab25d684ee83a9f82562a59234fb86b964bc4
macaraos <macarena.araos.p@gmail.com>
2017-11-12 22:58:41 -0300

    actualizando documentos html y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,38 @@
+//Darle funcionalidad al boton
 var botonLlamar = document.getElementById("btn");
 botonLlamar.addEventListener("click", rescatarMensaje);
 
-
+//Rescatando mensaje de ingreso
 function rescatarMensaje(){
 
 		var comment = document.getElementById("comment").value;
 		document.getElementById("comment").value = " ";
 
+		//Llamando a boton
 		var btn = document.getElementById("btn");
 
-
-
+		//Creando contenedor para mensajes
 		var mensaje = document.createElement("div");
 		mensaje.classList.add("divNuevo");
 
 		var texto = document.createElement("p");
 
+		//Llamando a contenedor de mensajes
 		var cont = document.getElementById("cont");
 		var textTwo = document.createTextNode(comment);
 
-
+		//Dandole a cada padre su hijo
 		texto.appendChild(textTwo);
 		mensaje.appendChild(texto);
 		cont.appendChild(mensaje);
 
 }
 
+//Hacer que cada vez que se escriba, se cuenten los caracteres
 var zonaComment = document.getElementById("comment");
 zonaComment.addEventListener("keydown", contador);
 
+//Contar en forma regresiva y que cambien de color segun cantidad de caracteres
 function contador(){
 		var contador = document.getElementById("contador");
 		var comment = document.getElementById("comment").value;
@@ -47,10 +51,11 @@ function contador(){
 			contador.style.color = "red";
 		}
 
+		//desactivar boton cuando no se ingresan caracteres o son superior a 140
 		if(caracteres == null || caracteres > 140){
 			btn.disabled = true;
 		}
-
+		//Activar el boton cuando se ingresan caracteres superiores a 0 e inferiores a 140
 		if(caracteres > 0 || caracteres < 140){
 			btn.disabled = false;
 		}
