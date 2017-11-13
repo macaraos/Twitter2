fdd7ea8ca5e11dbf0fe48b8d6d4982952c07153f
macaraos <macarena.araos.p@gmail.com>
2017-11-12 22:39:28 -0300

    actualizando twitter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,58 @@
-function agregar(){
-	var comentario = document.getElementById("comment").value;
+var botonLlamar = document.getElementById("btn");
+botonLlamar.addEventListener("click", rescatarMensaje);
 
-document.getElementById("comment").value = " ";
 
-var btn = document.getElementById("btn")
-var cont = document.getElementById("cont");
+function rescatarMensaje(){
 
-var newComments = document.createElement("div");
+		var comment = document.getElementById("comment").value;
+		document.getElementById("comment").value = " ";
 
-var textNewComment = document.createTextNode(comentario);
+		var btn = document.getElementById("btn");
 
-var contenedorElemento = document.createElement("p");
-var contador = document.getElementById("contador");
 
-contenedorElemento.appendChild(textNewComment);
-newComments.appendChild(contenedorElemento);
-cont.appendChild(newComments);
 
-};
+		var mensaje = document.createElement("div");
+		mensaje.classList.add("divNuevo");
+
+		var texto = document.createElement("p");
+
+		var cont = document.getElementById("cont");
+		var textTwo = document.createTextNode(comment);
+
+
+		texto.appendChild(textTwo);
+		mensaje.appendChild(texto);
+		cont.appendChild(mensaje);
+
+}
+
+var zonaComment = document.getElementById("comment");
+zonaComment.addEventListener("keydown", contador);
+
+function contador(){
+		var contador = document.getElementById("contador");
+		var comment = document.getElementById("comment").value;
+		var caracteres = comment.length;
+		contador.innerHTML = 140 - caracteres;
+
+		if(caracteres > 0 && caracteres < 120){
+			contador.style.color = "green";
+		}
+
+		if(caracteres >= 120 && caracteres < 130){
+			contador.style.color = "orange";
+		}
+
+		if(caracteres >= 130 && caracteres < 140){
+			contador.style.color = "red";
+		}
+
+		if(caracteres == null || caracteres > 140){
+			btn.disabled = true;
+		}
+
+		if(caracteres > 0 || caracteres < 140){
+			btn.disabled = false;
+		}
+
+}
